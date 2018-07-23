6bb9fcee926086eada7edbdaed31acef9ec81ed4
t <hello@singular.live>
2018-07-23 23:33:46 +0700

    feature: deploy interactive layer

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ var rmdir = require('rmdir');
 
 var WIDGET_DEPLOY_URL = 'https://app.singular.live/widgets/deploy';
 var APP_DEPLOY_URL = 'https://app.singular.live/apptemplates/deploy';
+// var IL_DEPLOY_URL = 'https://app.singular.live/interactives/deploy';
 // var WIDGET_DEPLOY_URL = 'http://localhost:3000/widgets/deploy';
 // var APP_DEPLOY_URL = 'http://localhost:3000/apptemplates/deploy';
+
+//var IL_DEPLOY_URL = 'http://localhost:3000/interactives/deploy';
+var IL_DEPLOY_URL = 'http://alpha.singular.live/interactives/deploy';
 
 // Get user arguments
 var userArgs = process.argv.slice(2);
@@ -22,10 +26,14 @@ if (!command) {
 
 function helpMe() {
   console.log('Available commands from singular-cli');
+
   console.log('singular createwidget <widget-name> - Init Singular Widget boiler plate');
   console.log('singular deploywidget <widget-folder-name> - Deploy Singular Widget');
+
   console.log('singular createapp <app-name> - Init Singular App boiler plate');
   console.log('singular deployapp <app-folder-name> - Deploy Singular App');
+
+  console.log('singular deployinteractive <interactive-folder-name> - Deploy Singular Interactive Layer');
 }
 // Shared functions
 
@@ -308,6 +316,95 @@ if (command.toLowerCase() == 'createwidget') {
       });
     }
   });
+}
+else if (command.toLowerCase() == 'deployinteractive') {
+
+  // Folder name is required
+  if (!userArgs[1]) {
+    console.log('Please use singular deployinteractive <interactive-name>');
+    return;
+  }
+
+  var folderName = userArgs[1];
+  var folderPrefix = './' + folderName + '/';
+
+  console.log('-----------------------------------------------');
+  console.log('Singular.Live Interactive Layer deploy');
+
+  try {
+    var config = fs.readFileSync(folderPrefix + 'deploykey.json', {encoding: 'utf8'});
+    var configJson = JSON.parse(config);
+    if (!configJson.deploykey) {
+      console.error('ERROR: Cannot find deploy key in deploykey.json');
+      return;
+    }
+  }
+  catch (e) {
+    console.error('ERROR: Cannot access deploykey.json: ' + e);
+    return;
+  }
+
+  console.log('Validating files in directory "source"');
+
+  // Check for output.html and icon.png
+  try {
+    var stats = fs.lstatSync(folderPrefix + 'source');
+
+    if (stats.isDirectory()) {
+      try {
+        var outputHtmlFile = fs.lstatSync(folderPrefix + 'source/interactivelayer.js');
+      } catch(e) {
+        console.error('ERROR: Cannot find file "source/interactivelayer.js"');
+        return;
+      }
+      try {
+        var iconPngFile = fs.lstatSync(folderPrefix + 'source/icon.png');
+      } catch(e) {
+        console.error('ERROR: Cannot find file "source/icon.png"');
+        return;
+      }
+    } else {
+      console.error('ERROR: "source" is not a directory');
+      return;
+    }
+  } catch(e) {
+    console.error('ERROR: Cannot find directory "source"');
+    return;
+  }
+
+  // Zip source folder
+  console.log('Creating zip file');
+
+  zipdir(folderPrefix + 'source', { saveTo: folderPrefix + 'SingularInteractive.zip' }, function (err, buffer) {
+    if (err) {
+      console.error('ERROR: Cannot zip directory "source"');
+      return;
+    } else {
+      console.log('Deploying Interactive Layer to Singular.Live');
+
+      // Upload source folder to Singular.Live
+      var req = xhr.put(IL_DEPLOY_URL);
+      req.field('key', configJson.deploykey)
+      req.attach('zipfile', folderPrefix + 'SingularInteractive.zip');
+      req.end(function(err, res) {
+        if (err) {
+          if(err.response && err.response.error && err.response.error.text) {
+            var errorJson = JSON.parse(err.response.error.text);
+            if (errorJson && errorJson.error && errorJson.error.message) {
+              console.log(errorJson.error.message);
+            }
+          }
+          console.error('Error with status: ' + err.status + '. Please try again later');
+        } else {
+          console.log('Interactive Layer ID: ' + res.body + ' successfully deployed');
+        }
+
+        // Cleanup
+        fs.unlink(folderPrefix + 'SingularInteractive.zip');
+      });
+    }
+  });
+
 } else {
   helpMe();
 }
